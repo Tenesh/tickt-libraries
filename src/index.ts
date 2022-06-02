a01a97bb2bb768a6ae4232c723b25c685b7b71e9
Tenesh <teneshdev@gmail.com>
2022-06-03 00:19:34 +0800

    Updates

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,4 @@ export * from './events/base-publisher';
 export * from './events/subjects/ticket-subject';
 export * from './events/tickets/ticket-created-event';
 export * from './events/tickets/ticket-updated-event';
+export * from './events/types/order-status';
